d899d249ec7bbab02517a1cd8534b3139eac7c5c
Roberto-Livi <robertomlivi@outlook.com>
2020-06-03 22:00:44 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,36 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
+
+    def user_attributes=(user_attr)
+      if user_attr[:username].present?
+        self.user = User.find_or_create_by(username: user_attr[:username])
+        # binding.pry
+      end
+    end
+
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  # Prevents from creating a category that already exists
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category unless category.name.empty?
+    end
+  end
 
 end
