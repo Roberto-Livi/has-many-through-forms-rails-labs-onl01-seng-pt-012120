2a02b222b1f451797d42f8d24eab126c908dbd8e
Roberto-Livi <robertomlivi@outlook.com>
2020-06-03 22:01:54 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,12 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
 
   # Prevents from creating a category that already exists
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
+      # Prevents creating a new category that is left blank
       self.categories << category unless category.name.empty?
     end
   end
